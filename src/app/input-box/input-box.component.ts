2fa567d8acc8601d9f211c4fdbb191b4e4bd32e3
Depyden <Posmetnijs@gmail.com>
2022-08-30 00:29:13 +0300

    15# Update date locale

--- src/app/input-box/input-box.component.ts
+++ src/app/input-box/input-box.component.ts
@@ -6,12 +6,38 @@ import {
   ViewEncapsulation,
 } from '@angular/core'
 import { InputBoxWordings } from '../wordings/input-box'
+import { MomentDateAdapter } from '@angular/material-moment-adapter'
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  DateAdapter,
+} from '@angular/material/core'
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'YYYY-MM-DD',
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'YYYY-MM',
+    dateA11yLabel: 'DD',
+    monthYearA11yLabel: 'YYYY-MM',
+  },
+}
 
 @Component({
   selector: 'app-input-box',
   templateUrl: './input-box.component.html',
   styleUrls: ['./input-box.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class InputBoxComponent implements OnInit {
   public title: string = localStorage.getItem('title') || ''
@@ -29,8 +55,14 @@ export class InputBoxComponent implements OnInit {
   }
 
   onDateChange($event: any) {
-    this.dateChangeEvent.emit($event)
-    localStorage.setItem('date', $event)
+    if ($event) {
+      const date = $event._isAMomentObject ? $event._d : $event
+      this.dateChangeEvent.emit(date)
+      localStorage.setItem('date', date)
+    } else {
+      this.dateChangeEvent.emit(undefined)
+      localStorage.removeItem('date')
+    }
   }
 
   constructor() {}
